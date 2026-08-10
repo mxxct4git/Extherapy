cfb2eea49f54897c20c7d1f1e981e44b13e40e7d
mxxct4git <386965035@qq.com>
2026-08-10 16:49:15 +1000

    scroll

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
@@ -6,9 +7,20 @@ import { Membership } from "./pages/Membership";
 import { Knowledge } from "./pages/Knowledge";
 import { Activities } from "./pages/Activities";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
